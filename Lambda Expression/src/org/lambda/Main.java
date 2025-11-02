f652bf658c5769336aa58c2c07539934f57ac5f3
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2025-11-02 12:09:40 +0530

    Implement lambda expression for sum calculation

--- Lambda Expression/src/org/lambda/Main.java
+++ Lambda Expression/src/org/lambda/Main.java
@@ -2,9 +2,35 @@ package org.lambda;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("My System starts");
 
-        MyInter myInter = new MyInterImplement();
-        myInter.sayHello();
+//        MyInter myInter = new MyInterImplement();
+//        myInter.sayHello();
+        
+        
+    	
+        
+//        MyInter i = new MyInter() {
+//			
+//			@Override
+//			public void sayHello() {
+//				// TODO Auto-generated method stub
+//				System.out.println("This is first anonymous class");
+//			}
+//		};
+//		i.sayHello();
+        
+        
+    	
+    	
+//        MyInter i = () -> {
+//        	System.out.println("This is my first time using lambda");
+//        };
+//        i.sayHello();
+    	
+    	
+    	
+    	sumInter suminter = (a, b) -> a + b;
+    	System.out.println(suminter.sum(2, 4));
+    	System.out.println(suminter.sum(8,  12));
     }
 }
