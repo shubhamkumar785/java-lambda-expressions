e326cb425e969ecb49646b7f9cd5d308cd65e218
Shubham Kumar <160641503+shubhamkumar785@users.noreply.github.com>
2026-01-07 20:32:17 +0530

    Implement MyInter interface in MyInterImplement class

--- Lambda Expression/src/org/lambda/MyInterImplement.java
+++ Lambda Expression/src/org/lambda/MyInterImplement.java
@@ -1,1 +1,11 @@
+package org.lambda;
+
+public class MyInterImplement implements MyInter {
+
+    @Override
+    public void sayHello() {
+        System.out.println("Hello from MyInterImplement!");
+    }
+}
+
 
